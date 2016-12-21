d8ab407d6b0f11c2408a9a9d8053e87c1e64930a
Federico Brigante <bfred-it@users.noreply.github.com>
2016-12-20 18:39:20 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-module.exports = on;
-module.exports.on = on;
-module.exports.off = off;
-
 function getElementsList(elements) {
 	if (typeof elements === 'string') {
 		elements = document.querySelectorAll(elements);
@@ -16,24 +12,16 @@ function getElementsList(elements) {
 	return elements;
 }
 
-function on(elements, types, listener, useCapture) {
-	types = types.split(' ');
+function run(action, elements, types, listener, useCapture) {
+	types = types.split(' ').filter();
 	elements = getElementsList(elements);
 	var i, l, m, n;
 	for (i = 0, l = elements.length; i < l; i++) {
 		for (m = 0, n = types.length; m < n; m++) {
-			elements[i].addEventListener(types[m], listener, useCapture);
+			elements[i][action](types[m], listener, useCapture);
 		}
 	}
 }
 
-function off(elements, types, listener, useCapture) {
-	types = types.split(' ');
-	elements = getElementsList(elements);
-	var i, l, m, n;
-	for (i = 0, l = elements.length; i < l; i++) {
-		for (m = 0, n = types.length; m < n; m++) {
-			elements[i].removeEventListener(types[m], listener, useCapture);
-		}
-	}
-}
+module.exports.on = module.exports = run.bind(null, 'addEventListener');
+module.exports.off = off.bind(null, 'removeEventListener');
